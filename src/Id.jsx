03d3088bcbce5bbd1150606d0cf570ddd8caffbb
Thiago Veronezi <tveronezi@gmail.com>
2022-03-16 13:09:21 -0400

    jss

--- src/Id.jsx
+++ src/Id.jsx
@@ -1,12 +1,20 @@
 import {Link} from "react-router-dom";
 import { useParams } from "react-router-dom";
 import {Layout} from "./Layout";
+import {createUseStyles} from 'react-jss'
+
+const useStyles = createUseStyles({
+    mydiv: {
+        backgroundColor: "pink",
+    }
+})
 
 export function Id() {
-    let params = useParams();
+    const params = useParams();
+    const classes = useStyles();
     return (
         <Layout>
-            <div className="grid place-content-center bg-gray-300 h-full">
+            <div className={"grid place-content-center bg-gray-300 h-full " + classes.mydiv}>
                 <span>page {params.id}</span>
                 <Link to="/">home</Link>
             </div>
