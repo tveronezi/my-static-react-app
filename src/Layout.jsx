b21198a83b177d816daf01d1f922adf62e7e6ffc
Thiago Veronezi <tveronezi@gmail.com>
2022-03-16 12:55:43 -0400

    loop

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,15 +2,14 @@ import {Link} from "react-router-dom";
 import './App.css';
 
 export function Layout({children}) {
+    const links = ['a', 'b', 'c', 'd', 'e'];
     return (
         <section className="h-screen flex flex-col">
-            <header className="bg-blue-800 h-{headerHeight}">header</header>
-            <nav className="p-4 text-lg bg-yellow-800 h-{navHeight}">
-                <Link className="underline" to="/a">a</Link>
-                <Link className="underline" to="/b">b</Link>
-                <Link className="underline" to="/c">c</Link>
-                <Link className="underline" to="/d">d</Link>
-                <Link className="underline" to="/e">e</Link>
+            <header className="bg-blue-800 h-44">header</header>
+            <nav className="p-4 text-lg bg-yellow-800 h-36 space-x-4">
+                {links.map((link) =>
+                    <Link className="underline" to={"/" + link}>{link}</Link>
+                )}
             </nav>
             <main className="grow overflow-auto">
                 {children}
